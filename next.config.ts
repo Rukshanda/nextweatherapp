d24f416c1e0ce260654aa34d884f854c4fc5c4a2
Rukshanda <rukshandanadir@gmail.com>
2024-11-13 02:44:51 +0500

    next-weather-app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_WEATHERBIT_API_KEY: process.env.NEXT_PUBLIC_WEATHERBIT_API_KEY,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
