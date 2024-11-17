6f537e746bd51aa1bbe934ab2c99f51c91b32cba
Rukshanda <73388109+Rukshanda@users.noreply.github.com>
2024-11-17 11:57:25 +0500

    Update Logo.tsx

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <div className='mds:flex none font-bold items-end w-full sm:w-[200px]'>
       <FaTemperatureThreeQuarters className='text-[2rem] lg:text-[2.5rem] xl:text-[3rem]' />
-      <span className='text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem]'>
+      <span className='text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] text-white'>
         Forecastly
       </span>
     </div>
