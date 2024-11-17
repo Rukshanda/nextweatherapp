526bab6bc978dd560dfe7cfb5e7e59e2041a3d94
Rukshanda <73388109+Rukshanda@users.noreply.github.com>
2024-11-17 12:18:49 +0500

    Update Logo.tsx

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,9 +3,9 @@ import { FaTemperatureThreeQuarters } from 'react-icons/fa6'
 
 function Logo() {
   return (
-    <div className='mds:flex none font-bold items-end w-full sm:w-[200px]'>
+    <div className='mds:flex none font-bold items-end w-full sm:w-[200px]  text-white'>
       <FaTemperatureThreeQuarters className='text-[2rem] lg:text-[2.5rem] xl:text-[3rem]' />
-      <span className='text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] text-white'>
+      <span className='text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem]'>
         Forecastly
       </span>
     </div>
